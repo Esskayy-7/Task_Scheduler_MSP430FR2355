f52544ff42740c8ef4e07b95adb6e2090cc306bb
esskayy7 <serikikayode075@gmail.com>
2026-03-17 01:09:44 +0300

    Remove CCS build artifacts and add .gitignore

--- Task_Scheduler/Debug/Application/Timer_Task.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-Application/Timer_Task.obj: ../Application/Timer_Task.c
-Application/Timer_Task.obj: C:/Users/Asus/workspace_v10/Task_Scheduler/Drivers/Timer.h
-Application/Timer_Task.obj: ../Application/Timer_Task.h
-
-../Application/Timer_Task.c: 
-C:/Users/Asus/workspace_v10/Task_Scheduler/Drivers/Timer.h: 
-../Application/Timer_Task.h: 
